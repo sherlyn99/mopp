6e7d6872d8b33cbbac9c68283bef6620e86e051c
sherlyn99 <57467251+sherlyn99@users.noreply.github.com>
2023-10-20 14:23:33 -0700

    Update mopp.py

--- mopp/mopp.py
+++ mopp/mopp.py
@@ -24,46 +24,18 @@ logging.basicConfig(
 def mopp():
     pass
 
-
 @mopp.command()
-@click.argument("name")
 @click.option('-m', '--metadata', required=True, help=DESC_MD)
 @click.option('-i', '--input', required=True, help=DESC_INPUT)
-def create_workflow(name, metadata, input):
+def workflow(metadata):
     # load metadata
-    _env.workflow_metadata = load_metadata(metadata)
+    md = load_metadata(metadata)
     logging.info('Metadata loaded')
     # parse input files
     if path.isdir(input):
         file_list = glob(input + '/*.fastq.gz')
     else: raise FileNotFoundError(input)
-
-    workflow_organization.initialize_workflow(name, input)
     
-
-@mopp.command()
-@mopp.argument("name")
-def activate(name):
-    workflow_list = os.listdir("./workflows")
-    if (input in workflow_list):
-        _env.workflow_env = name
-        with open(f"./workflows/{name}/{name}_metadata.txt", "r") as read_file:
-            _env.workflow_metadata = json.load(read_file)
-    else:
-        logging.error("trying to activate a workflow that doesn't exist")
-
-@mopp.command()
-@mopp.argument("name")
-def delete(name):
-    workflow_list = os.listdir("./workflows")
-    if (name in workflow_list):
-        _env.workflow_env = ""
-        rmtree(f"./workflows/{name}")
-    else:
-        logging.error("trying to delete a workflow that doesn't exist")
-
-
-
 #my_func2()
     #click.echo("This is the metag command")
 
