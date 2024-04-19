7992c4c25d3116f05869955a8768101594de53fd
sherlyn <57467251+sherlyn99@users.noreply.github.com>
2024-04-18 22:13:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+DESCRIPTION = ''
+
 setup(
     name='mopp',
-    version='0.1.0',
+    version=VERSION,
+    author='Sherlyn Weng (@sherlyn99)',
+    author_email='y1weng@ucsd.edu',
+    description=DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Click',
-        'pandas'
-    ],
+    install_requires=['Click','pandas'],
     entry_points={
         'console_scripts': [
             'mopp = mopp.mopp:mopp',
